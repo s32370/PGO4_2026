81ed5c8c615085ed2100abf5767598889092452a
Herman <s32370@pjwstk.edu.pl>
2026-03-31 12:44:31 +0200

    policy sample code added

---
+++ src/Policy.java
@@ -0,0 +1,24 @@
+public class Policy {
+
+
+    private String policyNumber
+    private String clientName
+    private double basePremium
+    private int riskLevel
+    private double vehicleValue
+    private boolean hasAlarm
+    private boolean claimFreeClient
+    private static int createdPolicyCount
+    private static final double ADMINISTRATIVE_FEE
+
+
+//    a constructor initializing all key fields,
+//    getters for report data,
+//    a method public double calculateFinalPremium(),
+//    a method public double calculateRenewalPremium(),
+//    a method public String getRiskSummary(),
+//    a static method public static int getCreatedPolicyCount(),
+//    an overridden toString(),
+//    an overridden equals() comparing objects by policyNumber.
+
+}
